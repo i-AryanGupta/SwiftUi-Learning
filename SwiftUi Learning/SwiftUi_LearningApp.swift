b0e25e8c54b9e514978c3e14509982763eb10f0e
i-AryanGupta <aryangupta6008@gmail.com>
2024-10-08 14:12:32 +0530

    Initial Commit

---
+++ SwiftUi Learning/SwiftUi_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_LearningApp.swift
+//  SwiftUi Learning
+//
+//  Created by Yashom on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
